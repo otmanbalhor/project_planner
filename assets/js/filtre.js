f9af7865caba47dee2ff847a8c543e4cf46dfb6c
otman <otman@DESKTOP-4RA473J.localdomain>
2024-01-10 16:03:15 +0100

    resol prob

--- assets/js/filtre.js
+++ assets/js/filtre.js
@@ -7,6 +7,9 @@ export function filter(categorie) {
     const cards = document.querySelectorAll('.container__article__card');
 
 
+    //
+    //FONCTION POUR TRI
+    //
     cards.forEach(function(card){
 
         const h2Card = card.querySelector(".container__article__card__haut__titre");
